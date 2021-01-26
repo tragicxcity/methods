b6a1ae0a7e929e0e7b4e57e1ac2ef508ecfccb3e
Blake.Foster <camblakefost35007@gmail.com>
2021-01-26 14:57:16 -0600

    methods

---
+++ methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is you name");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"{name}, welcome to your very own cyber adventure");
+
+            TwoChoices();
+
+        }
+
+        private static void TwoChoices()
+        {
+
+            string input = "";
+            Console.WriteLine("What will you do first a or b");
+            input = Console.ReadLine();
+            if (input == "a")
+            {
+                Console.WriteLine("a. turn on pc");
+            }
+            else
+            {
+                Console.WriteLine("b. turn on montior");
+            }
+
+        }
+    }
+}
